b8b6c7e1c6d0ea99240a54bcc46274479a742d1c
yambahadurpoudel <yambahadur.poudel@my.jcu.edu.au>
2020-05-19 17:27:00 +1000

    Initialization of plugin

---
+++ youtube-subscription.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:    	 Youtube Subscribers
+ * Plugin URI:  	http://yambahadurp.sgedu.site/
+ * Description:     Display Youtube subscribe button  and count 
+ * Author:            Yam Bahadur Poudel
+ * Author URI:       http://yambahadurp.sgedu.site/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
